abd31ed91aa0de39abbdb778a802e9d5453372e7
Chaitanya Prakash Bapat <chai.bapat@gmail.com>
2020-06-15 02:19:39 -0700

    Update latest build selection and log when one build fails (#26)
    * update logic for latest build selection from 8hr to 24hr from lambda trigger time
    
    * add logging for empty filtered builds to report failure
    
    * rename var to release_job_type and no need to pass global var to a function
    
    * update logic if job isn't run in the pipeline
    
    * fix local variable assignment issue
    
    * removed the unnecessary if, fix nitpick

--- services/jenkins-pipeline-monitor/handler.py
+++ services/jenkins-pipeline-monitor/handler.py
@@ -4,12 +4,14 @@ import json
 import logging
 import secret_manager
 
+from datetime import datetime, timezone
 from jenkinsapi.jenkins import Jenkins
 
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger('boto3').setLevel(logging.CRITICAL)
 logging.getLogger('botocore').setLevel(logging.CRITICAL)
 
+release_job_type = ['mxnet_lib/static', 'python/pypi']
 
 def get_jenkins_obj(secret):
     """
@@ -48,25 +50,31 @@ def get_build_date(timestamp):
     return timestamp.date()
 
 
-def is_latest_day_build(current_build, latest_build):
+def is_latest_day_build(current_build):
     current_build_timestamp = get_build_timestamp(current_build)
-    latest_build_timestamp = get_build_timestamp(latest_build)
-    # if current build is within 8 hours of the latest build
-    seconds_difference = (latest_build_timestamp-current_build_timestamp).total_seconds()
+    current_time_stamp = datetime.now().replace(tzinfo=timezone.utc)
+    # if current build is within 24 hours of the current time
+    seconds_difference = (current_time_stamp-current_build_timestamp).total_seconds()
     hour_difference = divmod(seconds_difference, 3600)[0]
-    if(hour_difference < 8):
+    if(hour_difference < 24):
         return True
     else:
         return False
 
 
 def get_latest_day_builds(job, latest_build_number):
-    latest_build = get_build_from_build_number(job, latest_build_number)
-    builds = [latest_build]
-    current_build_number = latest_build_number-1
+    """
+    Get all the builds that were triggered in the past 24 hours from the current time
+    i.e. the time when the Lambda function is triggered
+    :param job: Jenkins Job object
+    :param latest_build_number: latest build number from which to start checking
+    :result: List[builds]
+    """
+    builds = []
+    current_build_number = latest_build_number
     while True:
         current_build = get_build_from_build_number(job, current_build_number)
-        if is_latest_day_build(current_build, latest_build):
+        if is_latest_day_build(current_build):
             builds.append(current_build)
             current_build_number -= 1
         else:
@@ -78,20 +86,52 @@ def get_release_job_type(build):
     return build.get_params()['RELEASE_JOB_TYPE']
 
 
-def filter_by_desired_release_job_type(latest_day_builds, desired_release_job_type):
+def filter_by_release_job_type(latest_day_builds):
     filtered_builds = []
     for build in latest_day_builds:
-        if get_release_job_type(build) in desired_release_job_type:
+        if get_release_job_type(build) in release_job_type:
             filtered_builds.append(build)
     return filtered_builds
 
 
 def status_check(builds):
+    """
+    Check the status of the filtered builds
+    i.e. Check if all the required release job types are present in the pipeline
+    If a build from the list of desired release job types doesn't exist, log the failure
+    else check the status via Jenkins API and report accordingly
+    :param builds
+    """
+    # dictionary of the type release_job_type: count
+    # e.g. {'mxnet_lib/static':0, 'python/pypi':0}
+    global release_job_type
+    success_count = 0
+    release_job_type_dict = {el: 0 for el in release_job_type}
+
+    # iterate over the builds to count number of the desired release job types
     for build in builds:
+        build_release_job_type = get_release_job_type(build)
         if build.get_status() == 'SUCCESS':
-            logging.info(f'Successful build {get_release_job_type(build)} {build.get_number()}')
+            logging.info(f'Successful build {build_release_job_type} {build.get_number()}')
         else:
-            logging.info(f'Failure build {get_release_job_type(build)} {build.get_number()}')
+            logging.info(f'Failure build {build_release_job_type} {build.get_number()}')
+        release_job_type_dict[build_release_job_type] += 1
+
+    # iterate over the map of release_job_type: count
+    # if 'mxnet_lib/static':1 indicates static jobtype job ran in the pipeline
+    # else 'mxnet_lib/static':0 indicates static jobtype never ran -> log as failed
+    for release_job_type_name, release_job_type_count in release_job_type_dict.items():
+        if release_job_type_count == 0:
+            logging.info(f'Failure build {release_job_type_name}')
+        elif release_job_type_count == 1:
+            success_count += 1
+        else:
+            logging.info(f'{release_job_type} ran {release_job_type_count} times')
+    # if success_count = 2 [i.e. len of release_job_type], it means both static & pypi jobs have run
+    if success_count == len(release_job_type):
+        logging.info(f'All the required jobs ran')
+    else:
+        logging.info(f'1/more of the required jobs did not run')
 
 
 def get_cause(build):
@@ -123,9 +163,7 @@ def jenkins_pipeline_monitor():
     latest_day_builds = get_latest_day_builds(job, latest_build_number)
     logging.info(f'latest builds {latest_day_builds}')
     # filter latest day builds by desired build type a.k.a release job type
-    desired_release_job_type = ['mxnet_lib/static', 'python/pypi']
-
-    filtered_builds = filter_by_desired_release_job_type(latest_day_builds, desired_release_job_type)
+    filtered_builds = filter_by_release_job_type(latest_day_builds)
     logging.info(f'Builds filtered by desired release job type : {filtered_builds}')
 
     desired_cause = 'hudson.model.Cause$UpstreamCause'
