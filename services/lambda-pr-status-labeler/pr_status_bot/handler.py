ac6b6c60a386591d448c104f2fe07421995dffd4
ChaiBapchya <chai.bapat@gmail.com>
2020-09-12 16:12:12 -0700

    update license header in files

--- services/lambda-pr-status-labeler/pr_status_bot/handler.py
+++ services/lambda-pr-status-labeler/pr_status_bot/handler.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -14,6 +16,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
+# -*- coding: utf-8 -*-
 
 import os
 import boto3
