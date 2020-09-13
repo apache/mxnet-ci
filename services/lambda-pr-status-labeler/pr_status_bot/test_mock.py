6131b9df5b1528661ec1991da90b3704c91f3c74
ChaiBapchya <chai.bapat@gmail.com>
2020-09-12 18:22:01 -0700

    replace mock classes with actual PyGithub class

--- services/lambda-pr-status-labeler/pr_status_bot/test_mock.py
+++ services/lambda-pr-status-labeler/pr_status_bot/test_mock.py
@@ -18,6 +18,7 @@
 # under the License.
 
 # -*- coding: utf-8 -*-
+import github
 import json
 import os
 from PRStatusBot import PRStatusBot, GithubObj, FAILURE_STATE, PENDING_STATE, SUCCESS_STATE, PR_WORK_IN_PROGRESS_LABEL, PR_AWAITING_TESTING_LABEL, PR_AWAITING_REVIEW_LABEL, PR_AWAITING_RESPONSE_LABEL, PR_AWAITING_MERGE_LABEL, WORK_IN_PROGRESS_TITLE_SUBSTRING
@@ -35,49 +36,54 @@ def test_if_payload_is_non_PR(mocker):
 def test_if_pr_closed(mocker):
     payload = {'target_url': 'PR-1'}
     prsb = PRStatusBot(None, None, False)
-    mocker.patch.object(PRStatusBot, '_get_pull_request_object', return_value=MockPR(state='closed'))
+    mocker.patch.object(PRStatusBot, '_get_pull_request_object',
+                        return_value=get_mock_pr(state='closed'))
     actual = prsb.parse_payload(payload)
     expected = 2
     assert actual == expected
 
 
 def test_if_pr_wip_title(mocker):
-    mockpr = MockPR(title=WORK_IN_PROGRESS_TITLE_SUBSTRING)
+    mockpr = get_mock_pr(title=WORK_IN_PROGRESS_TITLE_SUBSTRING)
     prsb = PRStatusBot(None, None, False)
     mocker.patch.object(PRStatusBot, '_add_label')
     prsb._label_pr_based_on_status(FAILURE_STATE, mockpr)
-    PRStatusBot._add_label.assert_called_with(mockpr, PR_WORK_IN_PROGRESS_LABEL)
+    PRStatusBot._add_label.assert_called_with(
+        mockpr, PR_WORK_IN_PROGRESS_LABEL)
 
 
 def test_if_pr_draft(mocker):
-    mockpr = MockPR(draft=True)
+    mockpr = get_mock_pr(draft=True)
     prsb = PRStatusBot(None, None, False)
     mocker.patch.object(PRStatusBot, '_add_label')
     prsb._label_pr_based_on_status(SUCCESS_STATE, mockpr)
-    PRStatusBot._add_label.assert_called_with(mockpr, PR_WORK_IN_PROGRESS_LABEL)
+    PRStatusBot._add_label.assert_called_with(
+        mockpr, PR_WORK_IN_PROGRESS_LABEL)
 
 
 def test_if_ci_status_failure(mocker):
-    mockpr = MockPR()
+    mockpr = get_mock_pr()
     prsb = PRStatusBot(None, None, False)
     mocker.patch.object(PRStatusBot, '_add_label')
     prsb._label_pr_based_on_status(FAILURE_STATE, mockpr)
-    PRStatusBot._add_label.assert_called_with(mockpr, PR_WORK_IN_PROGRESS_LABEL)
+    PRStatusBot._add_label.assert_called_with(
+        mockpr, PR_WORK_IN_PROGRESS_LABEL)
 
 
 def test_if_ci_status_pending(mocker):
-    mockpr = MockPR()
+    mockpr = get_mock_pr()
     prsb = PRStatusBot(None, None, False)
     mocker.patch.object(PRStatusBot, '_add_label')
     prsb._label_pr_based_on_status(PENDING_STATE, mockpr)
-    PRStatusBot._add_label.assert_called_with(mockpr, PR_AWAITING_TESTING_LABEL)
+    PRStatusBot._add_label.assert_called_with(
+        mockpr, PR_AWAITING_TESTING_LABEL)
 
 
 def test_if_pr_no_reviews(mocker):
     def mock_no_review_counts(self, pr):
         # approves, request_changes, comments
         return 0, 0, 0
-    mockpr = MockPR()
+    mockpr = get_mock_pr()
     prsb = PRStatusBot(None, None, False)
     mocker.patch.object(PRStatusBot, '_add_label')
     mocker.patch.object(PRStatusBot, '_parse_reviews', mock_no_review_counts)
@@ -89,44 +95,21 @@ def test_if_pr_reviews_requested_changes(mocker):
     def mock_no_review_counts(self, pr):
         # approves, request_changes, comments
         return 0, 1, 0
-    mockpr = MockPR()
+    mockpr = get_mock_pr()
     prsb = PRStatusBot(None, None, False)
     mocker.patch.object(PRStatusBot, '_add_label')
     mocker.patch.object(PRStatusBot, '_parse_reviews', mock_no_review_counts)
     prsb._label_pr_based_on_status(SUCCESS_STATE, mockpr)
-    PRStatusBot._add_label.assert_called_with(mockpr, PR_AWAITING_RESPONSE_LABEL)
+    PRStatusBot._add_label.assert_called_with(
+        mockpr, PR_AWAITING_RESPONSE_LABEL)
 
 
-class MockRepo:
-    def __init__(self, name=None):
-        self.name = name
-
-    def get_commit(self, commit_sha):
-        return 'abc'
-
-    def get_pull(self, pr_number):
-        return MockPR(pr_number)
-
-
-class MockGithub:
-    def __init__(self):
-        pass
-
-    def get_repo(self, name=None):
-        return MockRepo(name)
-
-
-class MockLabel:
-    def __init__(self, name=None):
-        self.name = name
-
-
-class MockPR:
-    def __init__(self, pr_number=1, state='open', title='abc', draft=False):
-        self.number = pr_number
-        self.state = state
-        self.title = title
-        self.draft = draft
-
-    def get_labels(self):
-        return [MockLabel('lab1'), MockLabel('lab2')]
+def get_mock_pr(number=1, state='open', title='abc', draft=False):
+    mock_pr = github.PullRequest.PullRequest(requester='None',
+                                             headers='None',
+                                             attributes={'number': number,
+                                                         'state': state,
+                                                         'title': title,
+                                                         'draft': draft},
+                                             completed='None')
+    return mock_pr
